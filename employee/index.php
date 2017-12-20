6cce7f2b6cadedb60cfe223c07701a01add5a6ba
Sandeep969 <Sunnyandsandeep1996@gmail.com>
2017-12-20 17:57:57 +0530

    Add files via upload

---
+++ employee/index.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+	<style>
+	body { 
+  background: url('Images/main.jpg') no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+
+.container{
+padding: 25px;   
+}    
+    .space{
+    	padding-left:50px;
+    	height: 50px;
+    	width: 200px;
+
+    }
+    .emp{
+    	position: fixed;
+    	left: 40%;
+    	top:-2%;
+
+    }
+    .log{
+    	position: absolute;
+    	top:45%;
+    }
+    .log1{
+    	position: absolute;
+    	top:60%;
+    }
+	</style>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	
+</head>
+
+<body class="container">
+
+	<h1 class="text-danger"> Employee Stat</h1>
+	
+<a href="home.html"><button class="btn btn-danger space log">Manager Login </button></a>
+<a href="home.html"><button class="btn btn-success space log1">Employee Login </button></a>
+
+
+</body>
+
+
+
+
+
+<html>
